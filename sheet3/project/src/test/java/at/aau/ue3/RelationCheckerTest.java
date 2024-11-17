2e20458e9c7803dcf23898f57a60406c67764307
Angie Drucks <angie.drucks@gmail.com>
2024-11-17 17:44:15 +0100

    Added #2 test case with input (10,17) and return should be 34 - works.

--- sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
+++ sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
@@ -15,6 +15,11 @@ public class RelationCheckerTest {
         Assertions.assertEquals(0, result, "Expected result for (0,0) is 0");
     }
 
+    @Test
+    public void checkRelation_input_10_17_return_34() {
+        int result = checkRelation(10, 17);
+        Assertions.assertEquals(34, result, "Expected result for (10,17) is 42");
+    }
 
 
 
