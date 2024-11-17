5df1f9b36421c8505a9f7525fe10b8331ce23738
Angie Drucks <angie.drucks@gmail.com>
2024-11-17 17:56:50 +0100

    Added test case #4 for branch coverage with input(15,15) and return should be 30 - works.

--- sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
+++ sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
@@ -17,14 +17,23 @@ public class RelationCheckerTest {
     @Test
     public void checkRelation_input_10_17_return_34() {
         int result = checkRelation(10, 17);
-        Assertions.assertEquals(34, result, "Expected result for (10,17) is 42");
+        Assertions.assertEquals(34, result, "Expected result for (10,17) is 34");
     }
 
     // Branch Coverage #3
     @Test
     public void checkRelation_input_26_5_return_31() {
+        // Path: S1 > S2 > S3 > S4 > S5 > S10 > S13 > S15 (if -> else)
         int result = RelationChecker.checkRelation(26, 5);
-        Assertions.assertEquals(31, result, "Expected result for (26,5) is 28");
+        Assertions.assertEquals(31, result, "Expected result for (26,5) is 31");
+    }
+
+    // Branch Coverage #4
+    @Test
+    public void checkRelation_input_15_15_return_30() {
+        // Path: S1 > S2 > S3 > S7 > S8 > S10 > S11 > S15 (else -> if)
+        int result = checkRelation(15, 15);
+        Assertions.assertEquals(30, result, "Expected result for (15,15) is 30");
     }
 
 
