294938e0d545aec890e35924a08a461be94a09c2
Angie Drucks <angie.drucks@gmail.com>
2024-11-17 17:11:30 +0100

    Added #1 test case: input(0,0) and result should be 0

--- sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
+++ sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
@@ -5,6 +5,18 @@ import org.junit.jupiter.api.Test;
 
 public class RelationCheckerTest {
 
-    // TODO create tests
+    // Statement Coverage
+
+    @Test
+    public void checkRelation_input_0_0_return_0() {
+        int result = RelationChecker.checkRelation(0, 0);
+        Assertions.assertEquals(0, result, "Expected result for (0,0) is 0");
+    }
+
+
+
+
+
+
 
 }
