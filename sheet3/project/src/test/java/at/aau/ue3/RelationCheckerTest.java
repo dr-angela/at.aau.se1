46714492c27e21f8b028c527620faf2065e3bedd
Angie Drucks <angie.drucks@gmail.com>
2024-11-17 17:16:57 +0100

    See before

--- sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
+++ sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
@@ -3,13 +3,15 @@ package at.aau.ue3;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static at.aau.ue3.RelationChecker.*;
+
 public class RelationCheckerTest {
 
     // Statement Coverage
 
     @Test
     public void checkRelation_input_0_0_return_0() {
-        int result = RelationChecker.checkRelation(0, 0);
+        int result = checkRelation(0, 0);
         Assertions.assertEquals(0, result, "Expected result for (0,0) is 0");
     }
 
