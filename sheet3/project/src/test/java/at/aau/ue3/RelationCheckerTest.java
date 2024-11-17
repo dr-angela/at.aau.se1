6c29261ae44b509e3b66ff881a618eeed63af351
Angie Drucks <angie.drucks@gmail.com>
2024-11-17 18:09:29 +0100

    Changed input values for #1 - #4 so that path coverage is covered within those 4 testcases.

--- sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
+++ sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
@@ -7,43 +7,32 @@ import static at.aau.ue3.RelationChecker.*;
 
 public class RelationCheckerTest {
 
-    // Statement Coverage #1
+    // Statement Coverage #1 else -> else
     @Test
     public void checkRelation_input_0_0_return_0() {
         int result = checkRelation(0, 0);
         Assertions.assertEquals(0, result, "Expected result for (0,0) is 0");
     }
-    // Statement Coverage #2
+    // Statement Coverage #2 if -> if
     @Test
     public void checkRelation_input_10_17_return_34() {
         int result = checkRelation(10, 17);
         Assertions.assertEquals(34, result, "Expected result for (10,17) is 34");
     }
 
-    // Branch Coverage #3
+    // Branch Coverage #3 else -> if
     @Test
     public void checkRelation_input_26_5_return_31() {
-        // Path: S1 > S2 > S3 > S4 > S5 > S10 > S13 > S15 (if -> else)
         int result = RelationChecker.checkRelation(26, 5);
         Assertions.assertEquals(31, result, "Expected result for (26,5) is 31");
     }
 
-    // Branch Coverage #4
+    // Branch Coverage #4 if -> else
     @Test
     public void checkRelation_input_15_15_return_30() {
-        // Path: S1 > S2 > S3 > S7 > S8 > S10 > S11 > S15 (else -> if)
+
         int result = checkRelation(15, 15);
         Assertions.assertEquals(30, result, "Expected result for (15,15) is 30");
     }
-
-
-
-
-
-
-
-
-
-
 
 }
