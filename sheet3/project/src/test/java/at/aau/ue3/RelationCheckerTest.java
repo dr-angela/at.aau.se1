4bb1f54d4475573f4b06667903b8e326e414aa81
Angie Drucks <angie.drucks@gmail.com>
2024-11-17 17:50:39 +0100

    Added #3 test case for branch coverage with input(26,5) and result should be 31 - works.

--- sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
+++ sheet3/project/src/test/java/at/aau/ue3/RelationCheckerTest.java
@@ -7,19 +7,28 @@ import static at.aau.ue3.RelationChecker.*;
 
 public class RelationCheckerTest {
 
-    // Statement Coverage
-
+    // Statement Coverage #1
     @Test
     public void checkRelation_input_0_0_return_0() {
         int result = checkRelation(0, 0);
         Assertions.assertEquals(0, result, "Expected result for (0,0) is 0");
     }
-
+    // Statement Coverage #2
     @Test
     public void checkRelation_input_10_17_return_34() {
         int result = checkRelation(10, 17);
         Assertions.assertEquals(34, result, "Expected result for (10,17) is 42");
     }
+
+    // Branch Coverage #3
+    @Test
+    public void checkRelation_input_26_5_return_31() {
+        int result = RelationChecker.checkRelation(26, 5);
+        Assertions.assertEquals(31, result, "Expected result for (26,5) is 28");
+    }
+
+
+
 
 
 
