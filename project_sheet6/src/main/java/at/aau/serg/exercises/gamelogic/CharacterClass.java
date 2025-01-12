f2da546eec8a71642464bddd2fa8f51e75491212
Angie Drucks <angie.drucks@gmail.com>
2025-01-12 17:46:20 +0100

    Refactored Character class: Introduced Enums for characterClass

---
+++ project_sheet6/src/main/java/at/aau/serg/exercises/gamelogic/CharacterClass.java
@@ -0,0 +1,10 @@
+package at.aau.serg.exercises.gamelogic;
+
+/**
+ * Enum representing the possible character classes.
+ */
+public enum CharacterClass {
+    MAGE, // spellcaster
+    TANK, // protector with high defense
+    DAMAGE_DEALER // attacker with high offensive power
+}
