aadfe59051ae0b38323e03cd7cc85e939c4e5af1
Angie Drucks <angie.drucks@gmail.com>
2025-01-12 17:49:13 +0100

    Refactored Character class: Introduced Enums for damageType

---
+++ project_sheet6/src/main/java/at/aau/serg/exercises/gamelogic/DamageType.java
@@ -0,0 +1,10 @@
+package at.aau.serg.exercises.gamelogic;
+
+/**
+ * Enum representing the types of damage.
+ */
+public enum DamageType {
+    MELEE, // Nahkampf
+    RANGE, // Fernkampf
+    MAGIC // magic spells
+}
